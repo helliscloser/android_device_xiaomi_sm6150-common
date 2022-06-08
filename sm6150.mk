37e52c83cc9e06b5511ccfe962cb247c383ea36c
helliscloser <85108733+helliscloser@users.noreply.github.com>
2022-06-08 09:27:00 +0530

    rename overlay folder
    Update sm6150.mk
    Rename Overlay Folders

--- sm6150.mk
+++ sm6150.mk
@@ -266,7 +266,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-cherish
+    $(LOCAL_PATH)/overlay-corvus
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
