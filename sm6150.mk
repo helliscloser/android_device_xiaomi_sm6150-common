8b8ab4556dd0d1bdb8105f81aacea4f088c55634
helliscloser <mrumais@gmail.com>
2022-07-02 14:08:53 +0530

    Build MIUI Camera

--- sm6150.mk
+++ sm6150.mk
@@ -157,6 +157,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
 
+#Build MIUI Camera
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
